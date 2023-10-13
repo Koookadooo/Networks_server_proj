17e6d38df28ec166c009e5af42af49590e6adf0d
Koookadooo <fordriveraustin906@gmail.com>
2023-10-13 14:10:03 -0700

    Adding functionality to server to serve actual files

---
+++ helpers/MIME.py
@@ -0,0 +1,8 @@
+MIME_dict = {
+    '.txt': 'text/plain',
+    '.html': 'text/html',
+    '.pdf': 'application/pdf',
+    '.jpeg': 'image/jpeg',
+    '.gif': 'image/gif',
+    '.ico': 'image/x-icon'
+}
--- webserver.py
+++ webserver.py
@@ -1,14 +1,17 @@
 import sys
+import os
 import socket
+from helpers.MIME import MIME_dict
 
 if len(sys.argv) == 2:
     port = sys.argv[1]
 else:
     port = 28333
 
-response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 6\r\nConnection: close\r\n\r\nHello!\r\n"
-response = response.encode("ISO-8859-1")
+fnf_response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 13\r\nConnection: close\r\n\r\n404 not found\r\n"
+fnf_response = fnf_response.encode("ISO-8859-1")
 delim = b"\r\n\r\n"
+Content = MIME_dict
 
 with socket.socket() as s:
     s.bind(('', port))
@@ -21,6 +24,30 @@ with socket.socket() as s:
 
         while delim not in msg:
             msg += new_socket.recv(4096)
+
+        msg = msg.decode("ISO-8859-1")
+        lines = msg.split("\r\n")
+        get = lines[0].split()
+        file_ext = os.path.split(get[1])
+        file = file_ext[1]
+
+        ext = os.path.splitext(file)
+        mime_type = Content[ext[1]]
+
+        try:
+            with open("files\\"+file) as fp:
+                data = fp.read()
+        except Exception:
+            new_socket.sendall(fnf_response)
+            new_socket.close()
+            continue
+
+        data = data.encode("ISO-8859-1")
+        length = str(len(data))
+        data = data.decode("ISO-8859-1")
+
+        response = "HTTP/1.1 200 OK\r\nContent-Type: "+mime_type+"\rContent-Length: "+length+"\r\nConnection: close\r\n\r\n"+data+"\r\n"
+        response = response.encode("ISO-8859-1")
 
         new_socket.sendall(response)
         new_socket.close()
